d9bf0d5bcf49be34432edcd9b74ceccb4d486f31
Lev-TS <57505924+Lev-TS@users.noreply.github.com>
2020-05-08 23:40:38 +0300

    build deployed to surge.sh

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -3,11 +3,45 @@ import React from 'react';
 import './SearchBar.css';
 
 class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      term: ''
+    }
+    
+    this.search = this.search.bind(this);
+    this.handleTermChange = this.handleTermChange.bind(this);
+    this.handleEnter = this.handleEnter.bind(this);
+  }
+
+  search() {
+    this.props.onSearch(this.state.term);
+  }
+
+  handleEnter(event) {
+    if (event.keyCode === 13) {
+      this.search()
+    }
+  }
+
+  handleTermChange(event) {
+    this.setState({ term: event.target.value })
+  }
+
   render() {
     return (
       <div className="SearchBar">
-        <input placeholder="Enter A Song, Album, or Artist" />
-        <button className="SearchButton">SEARCH</button>
+        <input 
+          placeholder="Enter A Song, Album, or Artist"
+          onChange={this.handleTermChange}
+          onKeyDown={this.handleEnter}
+          tabIndex="0"
+        />
+        <button 
+          className="SearchButton"  
+          onClick={this.search}
+        >SEARCH</button>
       </div>
     )
   }
--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -7,12 +7,17 @@ class TrackList extends React.Component {
   render() {
     return (
       <div className="TrackList">
-        {this.props.tracks.map(
+        {this.props.tracks && this.props.tracks.map(
           track => {
-            return <Track 
-              track={track}
-              key = {track.id}
-            />
+            return ( 
+              <Track 
+                track={track}
+                key={track.id}
+                onAdd={this.props.onAdd}
+                onRemove={this.props.onRemove}
+                isRemoval={this.props.isRemoval}
+              />
+            )
           }
         )}
       </div>
